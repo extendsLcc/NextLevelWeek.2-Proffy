2a751fc3d38133e4a54b9f733e8e0f310916fe50
extendslcc <lucas.lcc@hotmail.com>
2020-08-09 22:07:13 -0300

    WIP: add base Teacher registering page

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrivel que você quer dar aulas." />
+        </div>
+    )
+}
+
+export default TeacherList
